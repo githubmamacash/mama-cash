40ab29ff4b91ed913e06fb24ec7a89d37f08e39e
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2024-06-11 18:19:58 -0400

    (feat) SVG map

--- src/components/client/HorizontalScroller.tsx
+++ src/components/client/HorizontalScroller.tsx
@@ -1,43 +1,46 @@
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { useEffect, useRef } from 'react';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect, useRef } from "react";
 
 export default function HorizontalScroller({ children }) {
+  const wrapperRef = useRef<HTMLDivElement | null>();
 
-    const wrapperRef = useRef<HTMLDivElement | null>();
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const ctx = gsap.context(() => {
+      const wrapper = wrapperRef.current.querySelector(
+        ".key-section",
+      ) as HTMLElement;
+      const container = wrapperRef.current.querySelector(
+        ".key-section-container",
+      ) as HTMLElement;
+      const scroller = wrapperRef.current.querySelector(
+        ".scroller",
+      ) as HTMLElement;
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger)
-        const ctx = gsap.context(() => {
-            const wrapper = wrapperRef.current.querySelector('.key-section') as HTMLElement
-            const container = wrapperRef.current.querySelector('.key-section-container') as HTMLElement
-            const scroller = wrapperRef.current.querySelector('.scroller') as HTMLElement
+      // scroller.style.gridAutoColumns = container?.offsetWidth / 3 + 'px';
+      gsap.to(scroller, {
+        x: () =>
+          wrapper?.offsetWidth -
+          scroller?.offsetWidth -
+          container?.offsetLeft * 2 -
+          32,
+        scrollTrigger: {
+          trigger: ".key-section-container",
+          pin: true,
+          invalidateOnRefresh: true,
+          anticipatePin: 0,
+          start: "-50 top",
+          end: "bottom top",
+          // markers: true,
+          scrub: true,
+        },
+      });
+    });
+    return () => {
+      ctx.revert();
+    };
+  }, []);
 
-            // scroller.style.gridAutoColumns = container?.offsetWidth / 3 + 'px';
-            gsap.to(scroller, {
-                x: () => wrapper?.offsetWidth - scroller?.offsetWidth - container?.offsetLeft * 2 - 32,
-                scrollTrigger: {
-                    trigger: ".key-section-container",
-                    pin: true,
-                    invalidateOnRefresh: true,
-                    anticipatePin: 0,
-                    start: '-50 top',
-                    end: 'bottom top',
-                    // markers: true,
-                    scrub: true
-                }
-            });
-
-            console.log('sss')
-        })
-        return () => {
-            ctx.revert();
-        }
-    }, [])
-
-    return (
-        <div ref={wrapperRef}>
-            {children}
-        </div>
-    )
+  return <div ref={wrapperRef}>{children}</div>;
 }
