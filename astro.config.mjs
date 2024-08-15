9c8907c5b7332db446b8b302e257be7f8aa2e7d3
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2024-08-15 14:32:20 -0400

    (feat) Static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
-import vercel from "@astrojs/vercel/serverless";
-
-
-const isStatic = process.env.PLATFORM == 'static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next(), tailwind(), react()],
-  output: isStatic ? "static" : "server",
-  adapter: isStatic ? undefined : vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  })
+  output: "static"
 });
